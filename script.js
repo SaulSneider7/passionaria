11c5450fdadbbf4022d64c007844b98c87cca307
SaulSneider7 <saulsneider@hotmail.com>
2024-09-11 01:30:48 -0500

    complete panel de punto de acceso

---
+++ script.js
@@ -0,0 +1,25 @@
+// Selección de los elementos del DOM necesarios para la funcionalidad del modal
+const BtnAbrirModal = document.querySelector("#btn_open_modal");
+const BtnCerrarModal = document.querySelector("#btn_close_modal");
+const Modal = document.querySelector("#modal");
+
+// Evento para abrir el modal cuando se hace clic en el botón "Ubicación del local"
+BtnAbrirModal.addEventListener("click", () => {
+    // Eliminar la clase de cierre y añadir la clase de apertura
+    Modal.classList.remove('hide');
+    Modal.classList.add('show');
+    Modal.showModal();
+});
+
+// Evento para cerrar el modal cuando se hace clic en el botón de cierre
+BtnCerrarModal.addEventListener("click", () => {
+    // Añadir la clase de cierre y eliminar la clase de apertura
+    Modal.classList.remove('show');
+    Modal.classList.add('hide');
+    // Esperar a que termine la animación de cierre antes de cerrar el modal
+    Modal.addEventListener('animationend', () => {
+        if (Modal.classList.contains('hide')) {
+            Modal.close();
+        }
+    }, { once: true });
+});
